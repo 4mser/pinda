757216b48d9dc905ae840edd63b4b81e73931c2c
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-29 22:55:20 -0300

    sabores

--- src/components/cartas.jsx
+++ src/components/cartas.jsx
@@ -7,94 +7,105 @@ import "swiper/css/navigation";
 // import required modules
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// Data for each SwiperSlide
+const sabores = [
+  {
+    href: "/carta",
+    image: "/images/sabores/natural.png",
+    title: "Natural",
+    subtitle: "",
+  },
+  {
+    href: "/tragos",
+    image: "/images/sabores/arandano.png",
+    title: "Arándano",
+    subtitle: "Lavanda",
+  },
+  {
+    href: "/sushi",
+    image: "/images/sabores/durazno.png",
+    title: "Durazno",
+    subtitle: "Poleo",
+  },
+  {
+    href: "/carta",
+    image: "/images/sabores/mango.png",
+    title: "Mango",
+    subtitle: "Jengibre",
+  },
+  {
+    href: "/carta",
+    image: "/images/sabores/frambuesa.png",
+    title: "Frambuesa",
+    subtitle: "Limón Menta",
+  },
+  {
+    href: "/carta",
+    image: "/images/sabores/piña.png",
+    title: "Piña",
+    subtitle: "Albahaca",
+  },
+  {
+    href: "/carta",
+    image: "/images/sabores/mandarina.png",
+    title: "Mandarina",
+    subtitle: "Maracuyá",
+  },
+  {
+    href: "/carta",
+    image: "/images/sabores/manzana.png",
+    title: "Manzana",
+    subtitle: "Chai",
+  },
+  {
+    href: "/carta",
+    image: "/images/sabores/maqui.png",
+    title: "Maqui",
+    subtitle: "Cacao",
+  },
+  {
+    href: "/carta",
+    image: "/images/sabores/murta.png",
+    title: "Murta",
+    subtitle: "Membrillo",
+  },
+
+  {
+    href: "/carta",
+    image: "/images/sabores/jazmin.png",
+    title: "Jazmin Melena",
+    subtitle: "de León",
+  },
+];
+
 export default function Cartas() {
   return (
     <main className="pt-3 left-0 w-full overflow-hidden px-4">
       <p className="py-2 font-medium text-slate-700">Sabores</p>
       <Swiper
         spaceBetween={12}
-        slidesPerView={5}
+        slidesPerView={5.3}
         centeredSlides={false}
-        pagination={{
-          clickable: true,
-        }}
+        pagination={{ clickable: true }}
         navigation={false}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <Link href={"/tragos"}>
-            <div className="overflow-hidden  rounded-full shadow-md border border-white/30  from-cyan-300 to-cyan-600">
-              <img
-                src="/images/sabores/arandano.png"
-                alt=""
-                className="w-full h-full object-cover rounded-md"
-              />
-            </div>
-            <p className="text-[9px] font-normal text-slate-700 py-1 text-center">
-              Arándano <br /> Lavanda
-            </p>
-          </Link>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Link href={"/sushi"}>
-            <div className="overflow-hidden  rounded-full shadow-md border border-white/30  from-red-600 to-red-800">
-              <img
-                src="/images/sabores/durazno.png"
-                alt=""
-                className="w-full h-full object-cover rounded-md"
-              />
-            </div>
-            <p className="text-[9px] font-normal text-slate-700 py-1 text-center">
-              Durazno <br /> Poleo
-            </p>
-          </Link>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Link href={"/carta"}>
-            <div className="overflow-hidden rounded-full  shadow-md  border border-white/30  from-yellow-300 to-green-200">
-              <img
-                src="/images/sabores/mango.png"
-                alt=""
-                className="w-full h-full object-cover rounded-md"
-              />
-            </div>
-            <p className="text-[9px] font-normal text-slate-700 py-1 text-center">
-              Mango <br /> Jengibre
-            </p>
-          </Link>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Link href={"/carta"}>
-            <div className="overflow-hidden rounded-full  shadow-md  border border-white/30  from-yellow-300 to-green-200">
-              <img
-                src="/images/sabores/frambuesa.png"
-                alt=""
-                className="w-full h-full object-cover rounded-md"
-              />
-            </div>
-            <p className="text-[9px] font-normal text-slate-700 py-1 text-center">
-              Frambuesa <br /> Limón Menta
-            </p>
-          </Link>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <Link href={"/carta"}>
-            <div className="overflow-hidden rounded-full  shadow-md  border border-white/30  from-yellow-300 to-green-200">
-              <img
-                src="/images/sabores/piña.png"
-                alt=""
-                className="w-full h-full object-cover rounded-md"
-              />
-            </div>
-            <p className="text-[9px] font-normal text-slate-700 py-1 text-center">
-              Piña <br /> Albahaca
-            </p>
-          </Link>
-        </SwiperSlide>
+        {sabores.map((sabor, index) => (
+          <SwiperSlide key={index}>
+            <Link href={sabor.href}>
+              <div className="overflow-hidden rounded-full shadow-md border border-white/30">
+                <img
+                  src={sabor.image}
+                  alt={sabor.title}
+                  className="w-full h-full object-cover rounded-md"
+                />
+              </div>
+              <p className="text-[9px] font-normal text-slate-700 py-1 text-center">
+                {sabor.title} <br /> {sabor.subtitle}
+              </p>
+            </Link>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </main>
   );
