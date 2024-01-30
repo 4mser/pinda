67732d4f1fa6ce0568197d8af97e6afd7ec5fee8
4mser <nicolas.moreno.sagan@gmail.com>
2024-01-30 01:00:14 -0300

    kom

--- src/components/anuncios.jsx
+++ src/components/anuncios.jsx
@@ -30,12 +30,23 @@ export default function Anuncios() {
       >
         <SwiperSlide className="px-4 ">
           <Link href={"/carta"}>
-            <div className="h-32 overflow-hidden rounded-lg    bg-gradient-to-br flex items-center from-zinc-800 to-transparent ">
+            <div className="h-44 overflow-hidden rounded-lg    bg-gradient-to-br flex items-center from-zinc-800 to-transparent ">
               <img
                 src="/images/funcional.png"
                 alt=""
                 className="w-full h-full object-cover"
               />
+              <p className="rounded-xl px-2 py-1 bg-slate-900 absolute right-7 top-4 text-white text-[10px] font-medium">
+                NUEVA LINEA FUNCIONAL
+              </p>
+              <p className="absolute right-8 top-14 text-right text-[14px] font-[500]">
+                Potencia tu equilibrio <br />y activa tu claridad
+              </p>
+              <p className="absolute right-8 top-28 text-right text-[10px] font-normal">
+                Fusionamos la fermentación ancestral con <br /> potentes
+                extractos fúngicos y botánicos, creando <br />
+                una súper bebida diseñada para maximizar tu bienestar.
+              </p>
             </div>
           </Link>
         </SwiperSlide>
