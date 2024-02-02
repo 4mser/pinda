37bcaed2342551ecc760b3fcabff33407a56d9b5
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-02 12:10:56 -0300

    ubicacion mapa

--- src/components/mapa.jsx
+++ src/components/mapa.jsx
@@ -8,10 +8,11 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 const Mapa = () => {
   const mapContainerRef = useRef(null);
+  let map; // Referencia al objeto mapa para usar fuera de useEffect
 
   useEffect(() => {
     // Inicializa el mapa
-    const map = new mapboxgl.Map({
+    map = new mapboxgl.Map({
       container: mapContainerRef.current,
       // Puedes elegir otro estilo de mapa aquí
       style: "mapbox://styles/4mser/clrzt2pyg01fu01p1hdqu1elg",
@@ -21,9 +22,35 @@ const Mapa = () => {
     return () => map.remove();
   }, []);
 
+  const setUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          map.flyTo({
+            center: [longitude, latitude],
+            essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+            zoom: 17,
+          });
+        },
+        (error) => {
+          console.error("Error al obtener la ubicación", error);
+        }
+      );
+    } else {
+      console.error("Geolocalización no soportada por este navegador.");
+    }
+  };
+
   return (
-    <section className="px-4 h-[670px] overflow-hidden">
+    <section className="px-4 h-[670px] mb-1 overflow-hidden">
       <h3 className="py-2 font-medium text-slate-700">Puntos de venta</h3>
+      <button
+        onClick={setUserLocation}
+        className="absolute bottom-10 right-6 mb-4 p-2 bg-blue-500 text-white rounded-full"
+      >
+        ubi
+      </button>
       <div
         ref={mapContainerRef}
         className="rounded-lg"
