34a54ee274d60f280f39417f2fd50f92b00b20c7
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-02 19:19:54 -0300

    mapa completo modal

--- src/components/mapa-completo.jsx
+++ src/components/mapa-completo.jsx
@@ -1,27 +1,84 @@
 "use client";
 
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
-
-// Establece tu Access Token de Mapbox aquí
+import puntosPinda from "../data/puntosPinda.js"; // Asegura que la ruta sea correcta
+import Modal from "./modal.jsx";
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 const MapaCompleto = () => {
   const mapContainerRef = useRef(null);
+  const [modalInfo, setModalInfo] = useState({ isOpen: false, data: null });
 
   useEffect(() => {
-    // Inicializa el mapa
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      // Puedes elegir otro estilo de mapa aquí
-      style: "mapbox://styles/4mser/clrzt2pyg01fu01p1hdqu1elg",
+      style: "mapbox://styles/4mser/cls57alt7026701o89e1naf6a",
+      center: [-73.2429564323007, -39.81805120276486],
+      zoom: 12,
     });
 
-    // Limpieza al desmontar el componente
+    map.on("load", () => {
+      map.addSource("puntosPinda", {
+        type: "geojson",
+        data: puntosPinda,
+      });
+
+      map.addLayer({
+        id: "puntos-circulos",
+        type: "circle",
+        source: "puntosPinda",
+        paint: {
+          "circle-radius": 8,
+          "circle-color": "#007cbf",
+        },
+      });
+
+      map.addLayer({
+        id: "puntos-texto",
+        type: "symbol",
+        source: "puntosPinda",
+        layout: {
+          "text-field": ["get", "location"],
+          "text-offset": [0, 1.2],
+          "text-anchor": "top",
+          "text-size": 12,
+        },
+        paint: {
+          "text-color": "#000000",
+        },
+      });
+
+      // Evento de clic para abrir el modal con la información del punto
+      map.on("click", "puntos-circulos", (e) => {
+        const properties = e.features[0].properties; // Obtiene las propiedades del puntoPinda
+        setModalInfo({ isOpen: true, data: properties }); // Actualiza el estado para abrir el modal
+      });
+
+      // Cambia el cursor a un puntero al pasar sobre los puntos
+      map.on("mouseenter", "puntos-circulos", () => {
+        map.getCanvas().style.cursor = "pointer";
+      });
+      map.on("mouseleave", "puntos-circulos", () => {
+        map.getCanvas().style.cursor = "";
+      });
+    });
+
     return () => map.remove();
   }, []);
 
-  return <div ref={mapContainerRef} style={{ minHeight: "100dvh" }} />;
+  return (
+    <>
+      <div ref={mapContainerRef} style={{ width: "100%", height: "100vh" }} />
+      {modalInfo.isOpen && (
+        <Modal
+          isOpen={modalInfo.isOpen}
+          onClose={() => setModalInfo({ isOpen: false, data: null })}
+          data={modalInfo.data}
+        />
+      )}
+    </>
+  );
 };
 
 export default MapaCompleto;
