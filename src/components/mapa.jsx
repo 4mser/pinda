3dad4a806a829867bf972feb1c9e9f8b6408616c
4mser <nicolas.moreno.sagan@gmail.com>
2024-02-02 12:23:28 -0300

    mapa

--- src/components/mapa.jsx
+++ src/components/mapa.jsx
@@ -2,17 +2,17 @@
 
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
+import Link from "next/link";
 
 // Establece tu Access Token de Mapbox aquí
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 const Mapa = () => {
   const mapContainerRef = useRef(null);
-  let map; // Referencia al objeto mapa para usar fuera de useEffect
 
   useEffect(() => {
     // Inicializa el mapa
-    map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       // Puedes elegir otro estilo de mapa aquí
       style: "mapbox://styles/4mser/clrzt2pyg01fu01p1hdqu1elg",
@@ -22,35 +22,16 @@ const Mapa = () => {
     return () => map.remove();
   }, []);
 
-  const setUserLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          map.flyTo({
-            center: [longitude, latitude],
-            essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-            zoom: 17,
-          });
-        },
-        (error) => {
-          console.error("Error al obtener la ubicación", error);
-        }
-      );
-    } else {
-      console.error("Geolocalización no soportada por este navegador.");
-    }
-  };
-
   return (
-    <section className="px-4 h-[670px] mb-1 overflow-hidden">
-      <h3 className="py-2 font-medium text-slate-700">Puntos de venta</h3>
-      <button
-        onClick={setUserLocation}
-        className="absolute bottom-10 right-6 mb-4 p-2 bg-blue-500 text-white rounded-full"
-      >
-        ubi
-      </button>
+    <section className="px-4 h-[670px] overflow-hidden">
+      <div className="w-full flex justify-between py-2 items-center">
+        <h3 className=" font-medium text-slate-700">Puntos de venta</h3>
+        <Link href={"/mapa"}>
+          <p className="text-slate-500 text-xs font-medium pt-1">
+            Ver mapa completo
+          </p>
+        </Link>
+      </div>
       <div
         ref={mapContainerRef}
         className="rounded-lg"
