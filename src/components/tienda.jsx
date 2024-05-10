a2ebc467bcb46b4a2f38506b1c465f260bd5aa7d
4mser <nicolas.moreno.sagan@gmail.com>
2024-05-10 18:09:53 -0400

    colors

--- src/components/tienda.jsx
+++ src/components/tienda.jsx
@@ -9,25 +9,25 @@ const productos = {
             nombre: "Natural",
             precio: "1.990",
             imagen: "/images/pindas/pinda_natural.png",
-            bg:"bg-gradient-to-br from-orange-100 to-yellow-200"
+            bg:"bg-gradient-to-br from-yellow-100 to-orange-200"
         },
         {
             nombre: "Arándano Lavanda",
             precio: "1.990",
             imagen: "/images/pindas/pinda_arandano_lavanda.png",
-            bg:"bg-gradient-to-br from-blue-100 to-cyan-200"
+            bg:"bg-gradient-to-br from-cyan-100 to-blue-200"
         },
         {
             nombre: "Mango Jengibre",
             precio: "1.990",
             imagen: "/images/pindas/pinda_mango_jengibre.png",
-            bg:"bg-gradient-to-br from-yellow-100 to-orange-200"
+            bg:"bg-gradient-to-br from-lime-100 to-yellow-200"
         },
         {
             nombre: "Maqui Cacao",
             precio: "1.990",
             imagen: "/images/pindas/pinda_maqui_cacao.png",
-            bg:"bg-gradient-to-br from-purple-100 to-pink-200"
+            bg:"bg-gradient-to-br from-purple-100 to-purple-200"
         },
         {
             nombre: "Piña Albahaca",
@@ -39,7 +39,7 @@ const productos = {
             nombre: "Frambuesa Limón Menta",
             precio: "1.990",
             imagen: "/images/pindas/pinda_frambuesa_limon_menta.png",
-            bg:"bg-gradient-to-br from-pink-100 to-pink-200"
+            bg:"bg-gradient-to-br from-pink-100 to-red-200"
         }
     ],
     packs: [
@@ -119,7 +119,7 @@ const ProductoModal = ({ producto, isOpen, onClose }) => {
 
                     >
                         <motion.div
-                            className="bg-white p-5 rounded-lg max-w-md"
+                            className="bg-white p-5 rounded-lg max-w-md bg-"
                             variants={modalVariants}
                             onClick={(e) => e.stopPropagation()} // Previene el cierre del modal al hacer clic en el contenido
                         >
