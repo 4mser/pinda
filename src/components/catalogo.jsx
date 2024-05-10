193d4ee8d69a1a52f6340eafd68022d34174674f
4mser <nicolas.moreno.sagan@gmail.com>
2024-05-10 17:11:31 -0400

    sa

--- src/components/catalogo.jsx
+++ src/components/catalogo.jsx
@@ -1,24 +1,12 @@
-// import React from 'react'
 
-// const Catalogo = () => {
-//   return (
-//     <section className="fixed bottom-5 bg-black/20  right-5 w-20 h-20 rounded-full flex justify-center items overflow-hidden">
-//             <a href="/banners/catalogo/pinda_catalogo.pdf" className='overflow-hidden flex justify-center items-center' download>
-//               Catalogo
-//             </a>
-//     </section>
-//   )
-// }
-
-// export default Catalogo
 
 import React from 'react'
 import SpinningText from './SpinningText'
 
 const Catalogo = () => {
   return (
-    <section className="fixed bottom-5 left-5 background border border-black/20  rounded-full flex justify-center items-center z-50 hover:scale-110 transition cursor-pointer">
-                <a href="/banners/catalogo/pinda_catalogo.pdf" className='overflow-hidden flex justify-center items-center bg-slate-500/50 w-full h-full rounded-full text-white/80' download>
+    <section className="fixed bottom-5 left-5 background border border-black/20  rounded-full flex justify-center items-center z-50 hover:scale-110 transition cursor-pointer xl:bottom-10 xl:left-9">
+                <a href="/banners/catalogo/pinda_catalogo.pdf" className='overflow-hidden flex justify-center items-center bg-slate-500/50 xl:bg-slate-800 w-full h-full rounded-full text-white/80' download>
                  
                   <SpinningText text=" - CATÁLOGO - CATÁLOGO - CATÁLOGO">
                     <p className='text-[16px]'>2024</p>
