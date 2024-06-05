df80992f4551db2b4948f3d67d4791463aef7ec8
4mser <nicolas.moreno.sagan@gmail.com>
2024-06-05 19:21:45 -0400

    footer y corfo 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,42 @@
+'use client'
 import React from 'react';
 import { BackgroundGradientAnimation } from './ui/background-gradient-animation';
 import Image from 'next/image';
-import Link from 'next/link';
 
 const Footer = () => {
   const links = [
-    { src: "/svg/instagram.svg", width: 31, height: 31, alt: "Instagram", href: "https://www.instagram.com/pinda_kombucha" },
-    { src: "/svg/wsp.svg", width: 32, height: 32, alt: "WhatsApp", href: "https://wa.me/974525079" },
-    { src: "/svg/mail.svg", width: 33, height: 33, alt: "Email", href: "mailto:contacto@pindakombucha.cl" },
+    {
+      src: "/svg/instagram.svg",
+      width: 31,
+      height: 31,
+      alt: "Instagram",
+      href: "https://www.instagram.com/pinda_kombucha",
+      mobileHref: "instagram://user?username=pinda_kombucha"
+    },
+    {
+      src: "/svg/wsp.svg",
+      width: 32,
+      height: 32,
+      alt: "WhatsApp",
+      href: "https://wa.me/974525079"
+    },
+    {
+      src: "/svg/mail.svg",
+      width: 33,
+      height: 33,
+      alt: "Email",
+      href: "mailto:contacto@pindakombucha.cl"
+    },
   ];
+
+  const handleLinkClick = (href, mobileHref) => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile && mobileHref) {
+      window.location.href = mobileHref;
+    } else {
+      window.open(href, "_blank", "noopener noreferrer");
+    }
+  };
 
   return (
     <div className='relative'>
@@ -22,15 +50,18 @@ const Footer = () => {
       <p className='absolute text-white z-10 left-1/2 -translate-x-1/2 top-24'>DESTAPA TU BIENESTAR</p>
       <div className='flex justify-between gap-3 absolute left-1/2 -translate-x-1/2 top-32 z-10 items-center'>
         {links.map((link, index) => (
-          <Link key={index} href={link.href} target="_blank" rel="noopener noreferrer">
+          <div
+            key={index}
+            onClick={() => handleLinkClick(link.href, link.mobileHref)}
+            className='hover:scale-110 hover:cursor-pointer'
+          >
             <Image 
               src={link.src}
               width={link.width}
               height={link.height}
               alt={link.alt}
-              className='hover:scale-110 hover:cursor-pointer transition-transform'
             />
-          </Link>
+          </div>
         ))}
       </div>
       <BackgroundGradientAnimation />
