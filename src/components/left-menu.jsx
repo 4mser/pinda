3c98ad9c19634f05f609ffe2b542700cd4db60e3
4mser <nicolas.moreno.sagan@gmail.com>
2024-06-05 19:40:50 -0400

    left menu

--- src/components/left-menu.jsx
+++ src/components/left-menu.jsx
@@ -5,11 +5,11 @@ import Link from 'next/link';
 const menuItems = [
   { href: "/", icon: "/icons/home-diamond.svg", text: "Inicio", color: "bg-slate-200" },
   { href: "/mapa", icon: "/icons/pushpin.svg", text: "Puntos de venta", color: "bg-red-200" },
-  { icon: "/icons/dizzy.svg", text: "Historia y Beneficios", color: "bg-yellow-200" },
-  { icon: "/icons/cocktail-glass.svg", text: "Coctelería", color: "bg-sky-200" },
-  { icon: "/icons/recycling.svg", text: "Reutilización y Sostenibilidad", color: "bg-green-200" },
-  { icon: "/icons/events.svg", text: "Eventos", color: "bg-purple-200" },
-  { icon: "/icons/contact.svg", text: "Nosotros", color: "bg-blue-200" },
+  { href: "#", icon: "/icons/dizzy.svg", text: "Historia y Beneficios", color: "bg-yellow-200" },
+  { href: "#", icon: "/icons/cocktail-glass.svg", text: "Coctelería", color: "bg-sky-200" },
+  { href: "#", icon: "/icons/recycling.svg", text: "Reutilización y Sostenibilidad", color: "bg-green-200" },
+  { href: "#", icon: "/icons/events.svg", text: "Eventos", color: "bg-purple-200" },
+  { href: "#", icon: "/icons/contact.svg", text: "Nosotros", color: "bg-blue-200" },
 ];
 
 const backdropVariants = {
@@ -29,17 +29,17 @@ const listItemVariants = {
     x: 0,
     transition: {
       type: "spring",
-      delay: i * 0.05,
-      duration: 0.7
+      delay: i * 0.04,
+      duration: 0.5
     }
   }),
   closed: (i) => ({
     opacity: 0,
-    x: "100%",
+    x: "-100%",
     transition: {
       type: "spring",
-      delay: i * 0.03,
-      duration: 1.5
+      delay: i * 0.01,
+      duration: 1
     }
   }),
 };
@@ -62,20 +62,18 @@ const LeftMenu = ({ openMenu, handleMenu }) => {
     <AnimatePresence>
       {openMenu && (
         <motion.div
-          className="fixed z-[100] left-0 top-0 w-full h-[100dvh]"
+          className="fixed z-[100] left-0 top-0 w-full h-[100vh]"
           variants={backdropVariants}
           initial="closed"
           animate="open"
           exit="closed"
           onClick={handleMenu}
         >
-          <ul
-            className="w-[80%] h-[100dvh] grid grid-rows-7 xl:grid-rows-5 gap-0 xl:grid-cols-3 xl:w-full xl:gap-5 xl:p-20"
-          >
+          <ul className="w-[80%] h-[100vh] grid grid-rows-7 xl:grid-rows-5 gap-0 xl:grid-cols-3 xl:w-full xl:gap-5 xl:p-20">
             {menuItems.map((item, index) => (
-              <Link href={item.href || "#"} key={index}>
+              <Link href={item.href} key={index} onClick={handleMenu}>
                 <motion.li
-                  className={`flex items-center overflow-scroll gap-3 cursor-pointer h-full p-4 xl:p-6 xl:rounded-3xl xl:hover:shadow-2xl xl:overflow-hidden ${item.color}`}
+                  className={`flex items-center gap-3 h-full p-4 xl:p-6 xl:rounded-3xl cursor-pointer ${item.color} ${hoverEnabled ? 'xl:hover:shadow-2xl' : ''}`}
                   variants={listItemVariants}
                   custom={index}
                   initial="closed"
